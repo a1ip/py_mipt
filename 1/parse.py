3e846dc8c763564c9e17ba43b2a8d06f3cc4aa2d
Mihail Selezniov <mihailselezniov@yandex.ru>
2020-10-07 02:33:38 +0300

    nlp

--- 1/parse.py
+++ 1/parse.py
@@ -1,19 +1,16 @@
 import requests
 from lxml import html
-from nltk.stem.snowball import SnowballStemmer
-stemmer = SnowballStemmer("russian")
 
 url_post = 'https://habr.com/ru/post/110078/'
 page = requests.get(url_post)
 tree = html.fromstring(page.content)
 
 title = tree.xpath('//title')
-title_text = title[0].text
-print(title_text)
-print([stemmer.stem(word) for word in title_text.split()])
+print(title[0].text)
 
-content = tree.xpath('//div[@id="post-content-body"]')
-#print(content[0].text_content())
+content_lsa = tree.xpath('//div[@id="post-content-body"]')
+post_lsa = content_lsa[0].text_content()
+#print(content_lsa[0].text_content())
 
 
 url_top_weekly = 'https://habr.com/ru/top/weekly/'
@@ -25,3 +22,19 @@ print(title[0].text)
 
 content = tree.xpath('//a[@class="post__title_link"]/@href')
 print(content)
+
+
+
+print('-'*10, 'NLP', '-'*10)
+
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+
+#print('Load stopwords')
+#import nltk
+#nltk.download('stopwords')
+
+stemmer = SnowballStemmer("russian")
+russian_stopwords = stopwords.words("russian")
+#print(post_lsa)
+print(' '.join([stemmer.stem(word) for word in post_lsa.split() if word not in russian_stopwords]))
